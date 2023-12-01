70d3d450507d8680e4d452a5647a3130bac7ad94
Serg-Ale <sergioalexandre0716@gmail.com>
2023-12-01 08:57:04 -0300

    atualizadao middleware de paginacao

--- helpers/pagination.js
+++ helpers/pagination.js
@@ -1,24 +1,26 @@
+function validarPagina(pagina) {
+  const numeroPagina = Number(pagina);
+  return !Number.isNaN(numeroPagina) && numeroPagina >= 1 ? numeroPagina : 1;
+}
+
+function validarQuantidade(quantidade) {
+  const numeroQuantidade = Number(quantidade);
+  return !Number.isNaN(numeroQuantidade) &&
+    numeroQuantidade >= 1 &&
+    numeroQuantidade <= 10
+    ? numeroQuantidade
+    : 10;
+}
+
 module.exports = function paginacao(model) {
   return async (req, res, next) => {
-    const { pagina, quantidade } = req.query;
-
-    let paginaAtual = 1;
-    if (pagina && !Number.isNaN(Number(pagina)) && Number(pagina) >= 1) {
-      paginaAtual = Number(pagina);
-    }
-
-    let quantidadePorPagina = 10;
-    if (
-      quantidade &&
-      !Number.isNaN(Number(quantidade)) &&
-      Number(quantidade) >= 1 &&
-      Number(quantidade) <= 10
-    ) {
-      quantidadePorPagina = Number(quantidade);
-    }
-
     try {
-      const result = {};
+      const { pagina, quantidade } = req.query;
+
+      // Validação dos parâmetros de página e quantidade
+      const paginaAtual = validarPagina(pagina);
+      const quantidadePorPagina = validarQuantidade(quantidade);
+
       const { count, rows: results } = await model.Model.findAndCountAll({
         limit: quantidadePorPagina,
         offset: (paginaAtual - 1) * quantidadePorPagina,
@@ -26,10 +28,12 @@ module.exports = function paginacao(model) {
 
       const totalPaginas = Math.ceil(count / quantidadePorPagina);
 
-      result.count = count;
-      result.quantidadePorPagina = quantidadePorPagina;
-      result.totalPaginas = totalPaginas;
-      result.results = results;
+      const result = {
+        count,
+        quantidadePorPagina,
+        totalPaginas,
+        results,
+      };
 
       res.resultadosPaginados = result;
       next();
